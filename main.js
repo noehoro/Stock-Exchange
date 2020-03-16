7203f853200388bfa01ae49fef78fbd1474cb5e4
noehoro <noehoro@gmail.com>
2020-03-16 12:01:46 +0200

    Show Results as list checkpoint

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 // Script
+function buttonClicked() {
+  fetchResults();
+}
+
+function fetchResults() {
+  //Get value from input
+  let searchInput = document.getElementById("search");
+  let ticker = searchInput.value;
+
+  //fetch Stocks from API
+  fetch(
+    `https://financialmodelingprep.com/api/v3/search?query=${ticker}&limit=10&exchange=NASDAQ`
+  ).then(response => {
+    if (response.ok) {
+      response.json().then(data => {
+        //Create new list
+        let list = "";
+        //for each element add a row
+        for (i of data) {
+          list += addRow(i);
+        }
+        //show the from list
+        displayResults(list);
+      });
+    }
+  });
+}
+
+//Create Take in JSON and create a row
+function addRow(i) {
+  return `<li class="parent"><span class="Name">${i.name}</span><span class="ticker">(${i.symbol})</span></li>`;
+}
+
+//Receive the list and print it
+function displayResults(list) {
+  let results = document.getElementById("results");
+  results.innerHTML = list;
+}
+
+// When Button is clicked
+let button = document.getElementById("button");
+button.addEventListener("click", buttonClicked);
