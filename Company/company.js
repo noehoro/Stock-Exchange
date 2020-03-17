b0d0b81a0577b1453a19feeb15cc6098c91c92f7
noehoro <noehoro@gmail.com>
2020-03-17 02:18:18 +0200

    Cleanup and half of company page

--- Company/company.js
+++ Company/company.js
@@ -1,31 +1,96 @@
-//Receive pram
-var urlParams = new URLSearchParams(window.location.search);
-let ticker = urlParams.get("symbol");
+//Script
 
-//set title
-document.getElementById("title").innerText = ticker;
+//button configuration
+function buttonConfig() {
+  let button = document.getElementById("button");
+  let searchInput = document.getElementById("search");
+  button.addEventListener("click", buttonClicked);
+  searchInput.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      buttonClicked();
+    }
+  });
+}
 
-let button = document.getElementById("button");
-let searchInput = document.getElementById("search");
-button.addEventListener("click", buttonClicked);
-searchInput.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    button.click();
-  }
-});
+//shows element
+function show(tag) {
+  document.getElementById(tag).classList.remove("hide");
+}
+
+//hides element
+function hide(tag) {
+  document.getElementById(tag).classList.add("hide");
+}
 
 //if the search is clicked, go back to the original website
-function buttonClicked(){
-    let searchInput = document.getElementById("search");
-    let input = searchInput.value;
-    location.href=`../index.html?query=${input}`;
+function buttonClicked() {
+  let searchInput = document.getElementById("search");
+  let input = searchInput.value;
+  location.href = `../index.html?query=${input}`;
+}
+
+//insert Tag Icon and Title
+function setTag(data) {
+  document.getElementById("title").innerText = data.symbol;
+  document.getElementById("icon").setAttribute("href", data.profile.image);
+}
+
+//insert image
+function setImage(data) {
+  let img = document.getElementById("img");
+  img.innerHTML = `<img src=${data.profile.image} alt= '${data.profile.companyName} name' style='height:100%'>`;
+}
+
+//insert name
+function setCompanyName(data) {
+  let name = document.getElementById("name");
+  name.innerHTML = data.profile.companyName;
+}
+
+//insert stock price
+function setPrice(data) {
+  let price = document.getElementById("price");
+  let changesPercentage = data.profile.changesPercentage;
+  price.innerHTML = `Stock price: $${data.profile.price} <span id="priceChange">${changesPercentage}</span>`;
+}
+
+//See if the price change is positive or negative and set the color accordingly
+function setPriceChangeColor(percent) {
+  let priceChangeTag = document.getElementById("priceChange");
+  if (percent[1] === "-") {
+    priceChangeTag.classList.add("text-danger");
+  } else if (percent[1] === "+") {
+    priceChangeTag.classList.add("text-success");
+  } else {
+    priceChangeTag.classList.add("text-secondary");
+  }
+}
+
+//fetch the results from API
+function fetchResults() {
+  //Get Params
+  let urlParams = new URLSearchParams(window.location.search);
+  let ticker = urlParams.get("symbol");
+  //fetch Stocks from the API
+  fetch(
+    `https://financialmodelingprep.com/api/v3/company/profile/${ticker}`
+  ).then(response => {
+    if (response.ok) {
+      response.json().then(data => {
+        setImage(data);
+        setTag(data);
+        setCompanyName(data);
+        setPrice(data);
+        setPriceChangeColor(data.profile.changesPercentage);
+      });
+    }
+  });
 }
 
 //This happens when the page is loaded
 window.onload = function() {
-  document
-    .getElementById("main")
-    .classList.add("animation-main");
+  document.getElementById("main").classList.add("animation-main");
+  fetchResults();
+  buttonConfig();
 };
--- main.js
+++ main.js
@@ -1,39 +1,73 @@
 // Script
 
-//receives the Params
-var urlParams = new URLSearchParams(window.location.search);
-let ticker = urlParams.get("query");
-let searchInput = document.getElementById("search");
+// Button Configuration
+function buttonConfig(searchInput) {
+  //define button
+  let button = document.getElementById("button");
 
-//if ticker isn't empty, then use it!
-if (ticker !== "") {
-  searchInput.value = ticker;
-  buttonClicked();
+  //when button is clicked
+  button.addEventListener("click", buttonClicked);
+
+  //When Enter is pressed
+  searchInput.addEventListener("keyup", function(event) {
+    let results = document.getElementById("results-container");
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element
+      buttonClicked();
+      //If there is something typed then look for it
+    } else if (this.value.length >= 1) {
+      buttonClicked();
+      //if nothing is typed then change the url and hide the results
+    } else if (this.value.length === 0) {
+      addState("");
+      results.classList.remove("animation-show-results");
+      results.classList.add("animation-hide");
+    }
+  });
 }
+
+//receives the Params from the URL
+function getParams() {
+  let urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get("query");
+}
+
+//returns the search tag
+function getSearchInput() {
+  return document.getElementById("search");
+}
+
+//show element
 function show(tag) {
   document.getElementById(tag).classList.remove("hide");
 }
 
+//hide element
 function hide(tag) {
   document.getElementById(tag).classList.add("hide");
 }
 
-//while typing change the value of the parm in the link
+//Edit the link so that is matches the search
 function addState(query) {
   window.history.pushState("", "", `/index.html?query=${query}`);
 }
 
+//Do this when the button is clicked
 function buttonClicked() {
   //defines and animates the results
   let results = document.getElementById("results-container");
   results.classList.remove("animation-hide");
   results.classList.add("animation-show-results");
-  show("spinner");
   hide("results");
-  fetchResults();
+  show("spinner");
+  //Go and fetch the results
+  fetchResults(getParams(), getSearchInput());
 }
 
-function fetchResults() {
+function fetchResults(ticker, searchInput) {
   //Get value from input
   ticker = searchInput.value;
   //so that the url updates
@@ -46,12 +80,13 @@ function fetchResults() {
       response.json().then(data => {
         //Create new list
         let list = document.createDocumentFragment();
+        //checks if there are any results, otherwise returns "no results"
         if (data.length !== 0) {
-          //for each element add a row
-          for (json of data) {
+          //for each element add a row, add to the document a new row
+          for (info of data) {
             var li = document.createElement("li");
             li.classList.add("parent");
-            li.innerHTML = addRow(json);
+            li.innerHTML = addRow(info);
             list.appendChild(li);
           }
         } else {
@@ -61,7 +96,7 @@ function fetchResults() {
           li.innerHTML = "No Results";
           list.appendChild(li);
         }
-        //show the from list
+        //show the resulting list
         hide("spinner");
         show("results");
         displayResults(list);
@@ -70,10 +105,11 @@ function fetchResults() {
   });
 }
 
-//Create Take in JSON and create a row
-function addRow(json) {
-  let link = `company/company.html?symbol=${json.symbol}`;
-  return `<span class="Name">${json.name}</span><a href=${link} class="ticker">(${json.symbol})</a>`;
+//Create Take in data in form of JSON and create a row
+function addRow(data) {
+  //links to different file
+  let link = `company/company.html?symbol=${data.symbol}`;
+  return `<span class="Name">${data.name}</span><a href=${link} class="ticker">(${data.symbol})</a>`;
 }
 
 //Receive the list and print it
@@ -83,31 +119,25 @@ function displayResults(list) {
   results.appendChild(list);
 }
 
-// When Button is clicked
-let button = document.getElementById("button");
-button.addEventListener("click", buttonClicked);
-searchInput.addEventListener("keyup", function(event) {
-  let results = document.getElementById("results-container");
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    button.click();
-    //If there is something typed then look for it
-  } else if (this.value.length >= 1) {
-    button.click();
-    //if nothing is typed then change the url and hide the results
-  } else if (this.value.length === 0) {
-    addState("");
-    results.classList.remove("animation-show-results");
-    results.classList.add("animation-hide");
+//Run at the beginning of the program, if the query ticker is empty then it runs it
+function checkTicker(ticker, searchInput) {
+  if (ticker !== "" && ticker !== null) {
+    searchInput.value = ticker;
+    buttonClicked();
+  } else {
+    return;
   }
-});
+}
 
-//This happens when the page is loaded
-window.onload = function() {
+//searchbar Animation
+function searchBarAnimation() {
   document
     .getElementById("search-bar-container")
     .classList.add("animation-show-search-bar");
+}
+//This happens when the page is loaded
+window.onload = function() {
+  searchBarAnimation();
+  buttonConfig(getSearchInput());
+  checkTicker(getParams(), getSearchInput());
 };
