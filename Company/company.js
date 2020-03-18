79134bd104f3260343d3aa556721a32b87de03b5
noehoro <noehoro@gmail.com>
2020-03-18 11:30:28 +0200

    Chart Example

--- Company/company.js
+++ Company/company.js
@@ -102,26 +102,25 @@ window.onload = function() {
   document.getElementById("main").classList.add("animation-main");
   fetchResults();
   buttonConfig();
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: "line",
+
+    // The data for our dataset
+    data: {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, 10, 5, 2, 20, 30, 45]
+        }
+      ]
+    },
+
+    // Configuration options go here
+    options: { maintainAspectRatio: false }
+  });
 };
-
-var ctx = document.getElementById("myChart").getContext("2d");
-var chart = new Chart(ctx, {
-  // The type of chart we want to create
-  type: "line",
-
-  // The data for our dataset
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: "My First dataset",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: [0, 10, 5, 2, 20, 30, 45]
-      }
-    ]
-  },
-
-  // Configuration options go here
-  options: { maintainAspectRatio: false }
-});
