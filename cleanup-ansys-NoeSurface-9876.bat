3acd53afefaf471460d20ebb995fe95c7f76f469
Noe Horowitz <noehoro@gmail.com>
2020-05-03 21:51:26 +0300

    Delete cleanup-ansys-NoeSurface-9876.bat

--- cleanup-ansys-NoeSurface-9876.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set LOCALHOST=%COMPUTERNAME%
-if /i "%LOCALHOST%"=="NoeSurface" (taskkill /f /pid 9576)
-if /i "%LOCALHOST%"=="NoeSurface" (taskkill /f /pid 9876)
-
-del /F cleanup-ansys-NoeSurface-9876.bat
